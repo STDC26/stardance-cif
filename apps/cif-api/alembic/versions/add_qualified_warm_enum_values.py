f1305668f71d86bf151b8962c784d783734c9452
Jason <jason@stardance.ai>
2026-04-11 10:31:58 -0600

    Update down_revision for alembic migration

--- apps/cif-api/alembic/versions/add_qualified_warm_enum_values.py
+++ apps/cif-api/alembic/versions/add_qualified_warm_enum_values.py
@@ -1,14 +1,14 @@
 """add qualified and warm to qds_qualification_status_enum
 
 Revision ID: a1b2c3d4e5f6
-Revises: 4f767890ce95
+Revises: 2a64e7afbe4c
 Create Date: 2026-04-11 00:00:00.000000
 """
 from typing import Sequence, Union
 from alembic import op
 
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = '4f767890ce95'
+down_revision: Union[str, Sequence[str], None] = '2a64e7afbe4c'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
